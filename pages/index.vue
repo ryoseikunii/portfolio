fd45a70c1f21e178b3306798ce8bea43dd8e170d
kunii <ryoseikunii@gmail.com>
2024-03-13 22:07:37 +0900

    パス修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <img src="~/assets/images/IMG_5597.JPG">
-    <img src ="/img/IMG_4944.JPG">
-    <img src ="/img/IMG_5094.JPG">
-    <img src ="/img/IMG_5154.JPG">
-    <img src ="/img/IMG_5097.JPG">
-    <img src ="/img/IMG_5151.JPG">
-    <img src ="/img/IMG_5115.JPG">
+    <img src ="~/assets/images/IMG_4944.JPG">
+    <img src ="~/assets/images/IMG_5094.JPG">
+    <img src ="~/assets/images/IMG_5154.JPG">
+    <img src ="~/assets/images/IMG_5097.JPG">
+    <img src ="~/assets/images/IMG_5151.JPG">
+    <img src ="~/assets/images/IMG_5115.JPG">
     <Tutorial />
   </div>
 </template>
