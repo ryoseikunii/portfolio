ae6c6b4bf43e0b94385149ea65c3d40ef6479f7e
kunii <ryoseikunii@gmail.com>
2024-05-28 19:38:39 +0900

    タイトル変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'portfolio',
+    title: 'Ryosei Kunii',
     htmlAttrs: {
       lang: 'en'
     },
