d27a0315ead230b98541de013196e530b7aad5f1
kunii <ryoseikunii@gmail.com>
2024-06-16 03:00:42 +0900

    dependenciesの全体的なバージョンアップ

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,12 @@
 <template>
   <div>
-    <img src="~/assets/images/IMG_5597.JPG">
-    <img src ="~/assets/images/IMG_4944.JPG">
-    <img src ="~/assets/images/IMG_5094.JPG">
-    <img src ="~/assets/images/IMG_5154.JPG">
-    <img src ="~/assets/images/IMG_5097.JPG">
-    <img src ="~/assets/images/IMG_5151.JPG">
-    <img src ="~/assets/images/IMG_5115.JPG">
     <Tutorial />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({
+import { defineComponent } from "vue";
+export default defineComponent({
   name: 'IndexPage'
-})
+});
 </script>
