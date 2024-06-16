091a983f5d0300f9e7b1f9ea080017af1f2a690c
kunii <ryoseikunii@gmail.com>
2024-06-16 20:36:58 +0900

    buildからgenerateへ

--- .output/server/chunks/build/headerNav-styles-1.mjs-_Qa03mi0.mjs
+++
@@ -1,4 +0,0 @@
-const headerNav_vue_vue_type_style_index_0_scoped_f6364a0e_lang = "nav[data-v-f6364a0e]{color:#fff}nav ul[data-v-f6364a0e]{display:flex;justify-content:flex-end}nav ul li[data-v-f6364a0e]{list-style-type:none;margin-left:15px}nav ul li a[data-v-f6364a0e]{color:#fff}";
-
-export { headerNav_vue_vue_type_style_index_0_scoped_f6364a0e_lang as h };
-//# sourceMappingURL=headerNav-styles-1.mjs-_Qa03mi0.mjs.map
